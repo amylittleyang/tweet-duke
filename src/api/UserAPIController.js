5aace684399e830354d4936a87e3aa463febd0c7
Amy Yang <jy152@duke.edu>
2016-11-10 16:49:09 -0500

    commit

--- src/api/UserAPIController.js
+++ src/api/UserAPIController.js
@@ -1,7 +1,7 @@
 module.exports = function (sequelize) {
   function homePageData (req,res,next) {
-  sequelize.query("QUERY GOES HERE",{ type: sequelize.QueryTypes.SELECT })
-  .then(function(users) {     
+  sequelize.query("SELECT * FROM thing",{ type: sequelize.QueryTypes.SELECT })
+  .then(function(users) {
     res.json(users);
     }, function(err) {
       res.status(500).send('Error getting homepage data.');
--- src/public/js/mainController.js
+++ src/public/js/mainController.js
@@ -1,3 +1,13 @@
 var mainController = angular.module('mainController',[]);
 mainController.controller('mainController',['$scope','$http',function($scope,$http) {
+  $scope.title = "Hello"
+  $http({
+    method: "GET",
+    url:"/api/1"
+  }).then( function(res) {
+    $scope.data1 = res.data;
+    $scope.data2 = res.data;
+  }, function (err) {
+    alert("error getting data.");
+  });
 }]);
