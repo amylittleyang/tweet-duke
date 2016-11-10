e3241cf8e7d19580d6051351a4ab9cc2f15761aa
Amy Yang <jy152@duke.edu>
2016-11-10 16:24:52 -0500

    commit

--- src/api/index.js
+++ src/api/index.js
@@ -2,9 +2,8 @@ import { Router } from 'express';
 export default ({sequelize}) => {
   let api = Router();
   var user = require('./UserAPIController')(sequelize);
-  // TODO: access control
-  
-  api.get('/:uid',user.homePageData);
 
+  api.get('/:uid',user.homePageData);
+  
   return api;
 }
